6b00ea8b8d002eb1dc06b95c16e58bed24f681de
CHA-SOYOUNG <chdo5412@naver.com>
2024-03-27 15:17:03 +0900

    dd

---
+++ prac/src/App.test.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'; // 리액트를 구현할 수 있는 플러그인을 연결
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Main from './component/Main';
+// JS파일에 외부 파일을 불러오는 것이기 때문에 "import" 키워드를 사용한다. 
+// 같은 JS파일은 확장자를 사용하지 않는다. 
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App; //다른 JS파일에서 불러올 수 있도록 내보내주기
